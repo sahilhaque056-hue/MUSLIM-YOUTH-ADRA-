a59d9d20509ef2903187c6842e152d71e3e3ec7b
sahilhaque056-hue <sahilhaque056@gmail.com>
2026-09-12 03:25:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+const form = document.getElementById("reportForm");
+const successMessage = document.getElementById("successMessage");
+const caseId = document.getElementById("caseId");
 
+form.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    // Generate a simple case number
+    const randomNumber = Math.floor(10000 + Math.random() * 90000);
+
+    const date = new Date();
+
+    const year = date.getFullYear();
+
+    const generatedCaseId =
+        "MYA-" + year + "-" + randomNumber;
+
+    caseId.textContent =
+        "Case ID: " + generatedCaseId;
+
+    // Hide form
+    form.style.display = "none";
+
+    // Show confirmation
+    successMessage.style.display = "block";
+
+    // Scroll to confirmation
+    successMessage.scrollIntoView({
+        behavior: "smooth",
+        block: "center"
+    });
+
+});
